bb2e491c871e34cb3ddcec4af8962261d0cc73f9
sorcerycss <sorcerycss@gmail.com>
2026-04-03 23:21:45 -0700

    feat: add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="footer">
+                available for work &nbsp;·&nbsp; 2026
+            </footer>
+        </>
+    )
+}
